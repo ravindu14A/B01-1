a568f525469a6e795387fe7df19b621714739c41
ravindu14A <bandaranet9@gmail.com>
2025-06-13 14:23:41 +0200

    Update predictions.py
    Minor change to visuals

--- results/predictions.py
+++ results/predictions.py
@@ -8,7 +8,7 @@ from scipy.stats import norm
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 
-def monte(country, station, N, years_predict, confidence_level, offset, pred_pos):
+def monte(country, station, N, years_predict, confidence_level, offset, pred_pos, unc_pos):
     ####----Processing----####
     alpha = 1 - confidence_level/100
     confidence = norm.ppf(1 - alpha / 2)
@@ -174,7 +174,7 @@ def monte(country, station, N, years_predict, confidence_level, offset, pred_pos
     )
 
     plt.text(
-        mean, pred_pos+8,  # x and y coordinates
+        mean, pred_pos+unc_pos,  # x and y coordinates
         f"Uncertainty: +/- {round(confidence_interval, 1)}",
         ha='center', va='bottom',
         fontsize=8,
